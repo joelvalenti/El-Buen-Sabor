f1f32b0c4183bd76da455414ff0c9f4a7c2f9eca
joelvalenti <valentijoel18@gmail.com>
2020-04-10 21:43:41 -0300

    #2 Principio de la Home Terminado

---
+++ src/app/components/modalregistro/modalregistro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modalregistro',
+  templateUrl: './modalregistro.component.html',
+  styleUrls: ['./modalregistro.component.css']
+})
+export class ModalregistroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
