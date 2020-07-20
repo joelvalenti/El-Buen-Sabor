7887a579b65ec38fc7de0d7e2c864d707cd14020
Fernando Rocha <f_erchu19@hotmail.com>
2020-07-19 21:37:05 -0300

    Added Link to AppRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CocinaComponent } from './pages/cocina/cocina.component';
 import { DeliveryComponent } from './pages/delivery/delivery.component';
+import { CajeroComponent } from './pages/cajero/cajero.component';
 
 const routes: Routes = [
   /*{ path : '', component: HomeComponent },
@@ -10,8 +11,8 @@ const routes: Routes = [
   { path: 'carrito', component: CarritoComponent},*/
   { path: 'delivery', component: DeliveryComponent },
   { path: 'cocina', component: CocinaComponent },
-  /*{ path: 'cajero', component: CajeroComponent},
-  { path: 'usuario', component: UsuarioComponent},
+  { path: 'cajero', component: CajeroComponent },
+  /*{ path: 'usuario', component: UsuarioComponent},
   { path: 'administrador', component: AdministradorComponent},
   { path: '**', component: Page404Component} //Esta siempre debe quedar al final.*/
 ];
