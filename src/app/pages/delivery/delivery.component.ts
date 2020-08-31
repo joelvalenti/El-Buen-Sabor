4f02006121587c73af6bcdb62c5dedd02ae99162
Fernando Rocha <f_erchu19@hotmail.com>
2020-08-31 19:20:10 -0300

    Se modifico algo

--- src/app/pages/delivery/delivery.component.ts
+++ src/app/pages/delivery/delivery.component.ts
@@ -48,7 +48,10 @@ export class DeliveryComponent implements OnInit {
   public ver(): void {
     if (this.usuario === null) {
       setTimeout(() => this.r(), 500);
-    } else if (this.usuario.rol.toLocaleLowerCase() !== 'delivery' && this.usuario.rol.toLocaleLowerCase() !== 'administrador') {
+    } else if (
+      this.usuario.rol.toLocaleLowerCase() !== 'delivery' &&
+      this.usuario.rol.toLocaleLowerCase() !== 'administrador'
+    ) {
       setTimeout(() => this.r(), 500);
     } else {
       this.cargarLocalidades();
